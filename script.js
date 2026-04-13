8a38a25f092bb1f2cb6eb6d2d17e6873114de1c7
Gabriel-santos-oliveira <santos.oliveira.gabriel2711@escola.pr.gov.br>
2026-04-13 08:16:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// Função para exibir mais informações na seção "Sobre"
-function mostrarInfo() {
-    const infoSobre = document.getElementById('info-sobre');
-    if (infoSobre.style.display === "none") {
-        infoSobre.style.display = "block";
-    } else {
-        infoSobre.style.display = "none";
-    }
-}
-
 // Função para ativar/desativar o modo escuro
 document.getElementById('darkModeBtn').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+// Função para abrir o modal
+function toggleModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Função para fechar o modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Função para alternar entre os slides do slider
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll(".image-slider .slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+}
